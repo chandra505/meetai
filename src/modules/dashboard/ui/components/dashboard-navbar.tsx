98c2e1eb41f428d64cae1504bad82c6eb316eef1
chandra500 <cmacharya@deerhold.com>
2025-07-20 17:04:12 +0545

    dashboard navbar completed

---
+++ src/modules/dashboard/ui/components/dashboard-navbar.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { SearchIcon } from "lucide-react";
+import { DashboardCommand } from "./dashboard-command";
+import { useEffect, useState } from "react";
+
+export const DashboardNavbar = () => {
+  const [commandOpen, setCommandOpen] = useState(false);
+
+  useEffect(() => {
+    const down = (e: KeyboardEvent) => {
+      if (e.key === "k" && (e.metaKey || e.ctrlKey)) {
+        e.preventDefault();
+        setCommandOpen((open) => !open);
+      }
+    };
+    document.addEventListener("keydown", down);
+    return () => document.removeEventListener("keydown", down);
+  }, []);
+
+  return (
+    <>
+      <DashboardCommand open={commandOpen} setOpen={setCommandOpen} />
+      <nav className="flex px-4 gap-x-2 items-center py-3 border-b bg-background">
+        <Button
+          className="h-9 w-[240px] justify-start font-normal text-muted-foreground hover:text-muted-foreground "
+          variant="outline"
+          size="sm"
+          onClick={() => {
+            setCommandOpen((open) => !open);
+          }}
+        >
+          <SearchIcon />
+          Search
+          <kbd className="ml-auto pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] text-muted-foreground ">
+            <span className="text-xs">&#8984;</span>k
+          </kbd>
+        </Button>
+      </nav>
+    </>
+  );
+};
